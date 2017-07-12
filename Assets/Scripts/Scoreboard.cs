e9808b4062e12f64fa8cd52a5017876542de37d7
ADCRM\rafu <rafu@ucsc.edu>
2017-07-12 15:48:52 -0700

    - Created scoreboard

---
+++ Assets/Scripts/Scoreboard.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scoreboard : MonoBehaviour {
+
+	public GameObject[] red_score_display;
+	public GameObject[] blue_score_display;
+
+	int red_score;
+	int blue_score;
+
+	// Use this for initialization
+	void Start () {
+		Reset ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void Reset()
+	{
+		red_score_display [0].GetComponent<TextMesh> ().text = "1";
+		red_score_display [1].GetComponent<TextMesh> ().text = "1";
+		blue_score_display [0].GetComponent<TextMesh> ().text = "1";
+		blue_score_display [1].GetComponent<TextMesh> ().text = "1";
+
+		red_score = 0;
+		blue_score = 0;
+	}
+
+	public void IncrementRedScore()
+	{
+		++red_score;
+		red_score_display [0].GetComponent<TextMesh> ().text = "" + red_score;
+		red_score_display [1].GetComponent<TextMesh> ().text = "" + red_score;
+	}
+
+	public void IncrementBlueScore()
+	{
+		++blue_score;
+		blue_score_display [0].GetComponent<TextMesh> ().text = "" + blue_score;
+		blue_score_display [1].GetComponent<TextMesh> ().text = "" + blue_score;
+	}
+}
