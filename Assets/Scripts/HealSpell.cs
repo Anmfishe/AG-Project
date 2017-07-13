ebdeec6477d9a6368ca34dc137870fa8ce3e540e
STREAM-MONSTER\Student <anmfishe@ucsc.edu>
2017-07-12 17:13:13 -0700

    Added heal spell.

---
+++ Assets/Scripts/HealSpell.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Heal spell
+// We need to heal every instance of player avatar.
+
+public class HealSpell: MonoBehaviour
+{
+   // int times_hit = 0;
+    public int healthAdded = 20;
+    GameObject target;
+
+    // Use this for initialization
+    void Start()
+    {
+        Destroy(gameObject, 5);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
