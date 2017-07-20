2e863bc30b3c0da885f85290b1240a009f8a126e
STREAM-MONSTER\Student <anmfishe@ucsc.edu>
2017-07-19 17:21:28 -0700

    hat put on head

--- Assets/Scripts/Holdable.cs
+++ Assets/Scripts/Holdable.cs
@@ -12,7 +12,6 @@ public class Holdable : MonoBehaviour
     public bool deactivated = false;
     public bool holdable = true;
     public bool memory = false;
-    public bool resettable = false;
     public bool countDown = false;
     public bool keyCard = false;
 
@@ -21,12 +20,7 @@ public class Holdable : MonoBehaviour
 
     public float padding = 50.0f;
 
-    //public guardAI guard1;
-
     public int index;
-    //float[] heights = { 0, 4 };
-  //  GameObject viveCam;
-    //public LevelStates levelStates;
     float startTime;
     float warpTimeBase;
     float warpTimeCap = 5.0f;
@@ -49,15 +43,8 @@ public class Holdable : MonoBehaviour
 
 
     rigidbody = GetComponent<Rigidbody>();
-       // viveCam = GameObject.FindGameObjectWithTag("Vive");
-        //viveCam.SetActive(false);
-        //heights[0] = viveCam.transform.position.y;
-        //levelStates = viveCam.GetComponent<LevelStates>();
+
         startTime = Time.time;
-        if (memory == true)
-        {
-            resettable = true;
-        }
         myStartPos = transform.position;
         startRot = transform.rotation.eulerAngles;
 
@@ -79,62 +66,27 @@ public class Holdable : MonoBehaviour
                 countDown = true;
                 warpTimeBase = Time.time;
             }
-            if (Time.time - warpTimeBase > warpTimeCap && resettable == true)
-            {
-                transform.position = myStartPos;
-                //transform.rotation.eulerAngle = startRot;
-                rigidbody.velocity = Vector3.zero;
-                rigidbody.angularVelocity = Vector3.zero;
-            }
+           
         }
 
         if (held == true && warped == false && memory == true)
         {
-           // startPos = viveCam.transform.position;
-           // startRot = viveCam.transform.localEulerAngles;
 
             if (fixedPos == false)
             {
-           //     levelStates.levelStates[index].SetActive(true);
-                //viveCam.transform.position = new Vector3(viveCam.transform.position.x, viveCam.transform.position.y + levelStates.levelStates[index].transform.position.y, viveCam.transform.position.z);
-            //    levelStates.levelStates[0].SetActive(false);
-                //print(levelStates.levelStates[index].active);
+    
                 warped = true;
             }
 
             else
             {
-                //foreach (Transform child in levelStates.levelStates[index].transform)
-                //    if (child.CompareTag("Landin"))
-                //    {
-                //        print("gotcha");
-                //        if (name == "1_Flashlight")
-                //        {
-                //            print("here");
-                //            transform.GetChild(0).gameObject.SetActive(true);
-                //        }
-
-                //        landingPoint = child.transform;
-
-                //        levelStates.levelStates[index].SetActive(true);
-                //        viveCam.transform.position = new Vector3(landingPoint.position.x, landingPoint.position.y, landingPoint.position.z);
-                //        levelStates.levelStates[0].SetActive(false);
-                //        //print(levelStates.levelStates[index].active);
-                //        warped = true;
-                //    }
+              
             }
         }
 
         if (deactivated == false && Time.time - startTime > .5 && memory == true)
         {
             int i = 0;
-            //foreach (GameObject state in levelStates.levelStates)
-            //{
-            //  //  levelStates.levelStates[i].SetActive(false);
-            //   // levelStates.levelStates[0].SetActive(true);
-            //    ++i;
-            //}
-           // viveCam.SetActive(true);
             deactivated = true;
         }
  
@@ -144,18 +96,9 @@ public class Holdable : MonoBehaviour
         //thrown = true;
         if (thrown == true)
         {
-           // if rest
-
-            //print("HITTTTTT");
             if (collision.transform.CompareTag("Water"))
             {
-                //print("bounce");
-                //  print(rigidbody.velocity);
-                //rigidbody.AddForce(Vector3.up * force);
-
-
-
-                // Bouncy shit here
+             
                 float modifier = Random.Range(-0.3f, 0.3f);
                 audioSource.pitch = 1 + modifier;
                 audioSource.PlayOneShot(bounceSound,1.0f);
@@ -171,28 +114,8 @@ public class Holdable : MonoBehaviour
                     "\nHigh Limit: " + (goldenAngle + padding));
                 if ((angle > (goldenAngle - padding)) && (angle < (goldenAngle + padding)))
                 {
-
-                    /*
-                    if (angle < 20)
-                    {
-                        rigidbody.AddForce(Vector3.up * force);
-                    }
-
-                    else
-                    {
-                    }*/
                     rigidbody.velocity = new Vector3(rigidbody.velocity.x, -rigidbody.velocity.y * .75f, rigidbody.velocity.z);   
-                // rigidbody.AddForce(Vector3.up * force);
                 }
-
-
-                // rigidbody.AddForce(Vector3.up * force);
-
-
-
-
-
-
 
             }
             else
---
+++ Assets/Scripts/hat_put.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class hat_put : MonoBehaviour {
+
+    // Detect the collision of hat and head
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag == "put")
+        {
+          gameObject.transform.SetParent(other.gameObject.transform);
+            gameObject.GetComponent<Rigidbody>().isKinematic = true;
+
+            // Search for the child hat in player
+            foreach (Transform child in other.transform) if (child.CompareTag("findHat"))
+            {
+                    gameObject.transform.position = child.transform.position;
+                    gameObject.transform.rotation = child.transform.rotation;
+                    //gameObject.transform.scale = child.transform.scale;
+                }
+
+        }
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
