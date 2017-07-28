0f3234a126f3cbfdf5a8b0bdeacf621c01853f0c
dylfaust <mdfaust@ucsc.edu>
2017-07-28 02:02:14 -0700

    Meteor fixed!

--- Assets/Scripts/MeteorSpell.cs
+++ Assets/Scripts/MeteorSpell.cs
@@ -2,8 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
 public class MeteorSpell : MonoBehaviour
 {
+
+	private Edwon.VR.VRGestureRig spellcastingGesture;
 	private PhotonView photonView;
 	private LineRenderer line;
 	public GameObject reticle;
@@ -11,76 +17,81 @@ public class MeteorSpell : MonoBehaviour
 	private Vector3 steeringDirection;
 	private Vector3[] points = new Vector3[2];
 	public bool mine = false;
-    public GameObject explosion;
+	public GameObject explosion;
+
+
+	public LayerMask targettable;
 	public GameObject wand;
-    public int damage = 20;
+	public int damage = 20;
 	private float castDist = 10;
 	private float magCap = 10;
 	private float skyCap = 10;
 	private float groundCap = 40;
-    public float duration = 12f;
-    public Vector3 direction; //Sets the direction to where the fireball is traveling to.
-    public float speed = 10f;
-    public float startup = 0.25f;
-    public float minLinearVelocity = 5f;
-    public float minAngularVelocity = 20f;
+	public float duration = 12f;
+	public Vector3 direction; //Sets the direction to where the fireball is traveling to.
+	public float speed = 10f;
+	public float startup = 0.25f;
+	public float minLinearVelocity = 5f;
+	public float minAngularVelocity = 20f;
 	private float steeringForceSky = 10000;
 	private float steeringForceGround = 15000;
 	public Rigidbody rb;
 
-    public AudioSource audioSource;
-    public AudioClip deflectAudio;
-    [HideInInspector]
-    public bool isMaster;
-    [HideInInspector]
-    public bool isSlave;
+	public AudioSource audioSource;
+	public AudioClip deflectAudio;
+	[HideInInspector]
+	public bool isMaster;
+	[HideInInspector]
+	public bool isSlave;
 
-//    [SerializeField]
-    private float activeTimer = 0;
-//    [SerializeField]
-    private SphereCollider fbCollider;
-    // Use this for initialization
-    void Start()
-    {
+	//    [SerializeField]
+	private float activeTimer = 0;
+	//    [SerializeField]
+	private SphereCollider fbCollider;
+	// Use this for initialization
+	void Start()
+	{
 		photonView = GetComponent<PhotonView> ();
 		if (photonView.isMine) 
 		{
 			reticleInstance = Instantiate (reticle, transform.position, transform.rotation) as GameObject;
 			mine = true;
 			wand = GameObject.Find ("Controller (right)");
+			spellcastingGesture = Camera.main.transform.parent.GetComponent<Edwon.VR.VRGestureRig> ();
+			spellcastingGesture.enabled = false;
 			rb = GetComponent<Rigidbody> ();
 			rb.AddForce (transform.forward * 1000);
 			line = GetComponent<LineRenderer> ();
 
 
 		}
-			//Destroy(this.gameObject, duration);
-			if (startup > 0)
-				activeTimer = startup;
-			if (fbCollider == null)
-				fbCollider = this.GetComponent<SphereCollider> ();
-			if (audioSource == null)
-				audioSource = this.GetComponent<AudioSource> ();
-    }
+		//Destroy(this.gameObject, duration);
+		if (startup > 0)
+			activeTimer = startup;
+		if (fbCollider == null)
+			fbCollider = this.GetComponent<SphereCollider> ();
+		if (audioSource == null)
+			audioSource = this.GetComponent<AudioSource> ();
+	}
 
-    // Update is called once per frame
-    void Update()
-    {
+	// Update is called once per frame
+	void Update()
+	{
 		if (mine == true) {
-			
+
 		}
 
-        //Timer to activate collider.
-        if (activeTimer > 0)
-            activeTimer -= Time.deltaTime;
-        else if (!fbCollider.enabled)
-            fbCollider.enabled = true;
+		//Timer to activate collider.
+		if (activeTimer > 0)
+			activeTimer -= Time.deltaTime;
+		else if (!fbCollider.enabled)
+			fbCollider.enabled = true;
 
-        //this.GetComponent<Rigidbody>().AddForce(this.transform.forward * speed * Time.deltaTime, ForceMode);
-        //this.GetComponent<Rigidbody>().velocity = this.transform.forward * speed;
-    }
-    private void FixedUpdate()
-    {
+		//this.GetComponent<Rigidbody>().AddForce(this.transform.forward * speed * Time.deltaTime, ForceMode);
+		//this.GetComponent<Rigidbody>().velocity = this.transform.forward * speed;
+	}
+	private void FixedUpdate()
+	{
 		// If this fireball belongs to me
 		if (mine == true)
 		{
@@ -97,7 +108,9 @@ public class MeteorSpell : MonoBehaviour
 			//Debug.DrawRay(wand.transform.position, wand.transform.forward * 1000, Color.red, 0.01f);
 			//Get raycast results.
 
-			if (Physics.Raycast (wand.transform.position, wand.transform.forward, out hit, 1000)) {
+			if (Physics.Raycast (wand.transform.position, wand.transform.forward, out hit, 1000,targettable) && Vector3.Distance(wand.transform.position, hit.point) < 40) {
+
+				print(Vector3.Distance(wand.transform.position, hit.point));
 				if (hit.transform.gameObject.tag == ("Spell")) 
 				{
 					return;
@@ -119,7 +132,7 @@ public class MeteorSpell : MonoBehaviour
 
 			Debug.DrawRay (wand.transform.position, fwd * 1000, Color.green);
 		}
-    }
+	}
 
 	Vector3 clampVector(Vector3 myVector)
 	{
@@ -134,10 +147,10 @@ public class MeteorSpell : MonoBehaviour
 		return myVector;
 	}
 
-    private void OnCollisionEnter(Collision collision)
-    {
-        GameObject other = collision.gameObject;
-        print("Collided by " + other.name);
+	private void OnCollisionEnter(Collision collision)
+	{
+		GameObject other = collision.gameObject;
+		print("Collided by " + other.name);
 
 		if (other.CompareTag ("Player")) {
 			print ("hit the body");
@@ -184,19 +197,19 @@ public class MeteorSpell : MonoBehaviour
 		} 
 		else if (other.CompareTag ("BluePlatform") || other.CompareTag ("RedPlatform") || other.CompareTag ("Stage")) 
 		{
-				//Instantiate new explosion.
-				GameObject newExplosion = PhotonNetwork.Instantiate (explosion.name, this.transform.position, new Quaternion (), 0);
+			//Instantiate new explosion.
+			GameObject newExplosion = PhotonNetwork.Instantiate (explosion.name, this.transform.position, new Quaternion (), 0);
 
-				//Delete this game object.
-				DestroyFireball ();
+			//Delete this game object.
+			DestroyFireball ();
 
 		}
-    }
+	}
 
 	private void OnCollisionStay(Collision collision)
 	{
 		GameObject other = collision.gameObject;
-	if (other.CompareTag ("BluePlatform") || other.CompareTag ("RedPlatform") || other.CompareTag ("Stage")) 
+		if (other.CompareTag ("BluePlatform") || other.CompareTag ("RedPlatform") || other.CompareTag ("Stage")) 
 		{
 			//Instantiate new explosion.
 			GameObject newExplosion = PhotonNetwork.Instantiate (explosion.name, this.transform.position, new Quaternion (), 0);
@@ -208,43 +221,44 @@ public class MeteorSpell : MonoBehaviour
 	}
 
 
-    private void OnTriggerEnter(Collider collider)
-    {
-        GameObject other = collider.gameObject;
-        print("Triggered (heh) by " + other.name);
-        if (other.CompareTag("SpellHitter"))
-        {
-            print("triggered spellhitter");
-            //Create reflected fireball if it was hit hard enough by the spell hitter.
-            Rigidbody otherBody = other.GetComponent<Rigidbody>();
+	private void OnTriggerEnter(Collider collider)
+	{
+		GameObject other = collider.gameObject;
+		print("Triggered (heh) by " + other.name);
+		if (other.CompareTag("SpellHitter"))
+		{
+			print("triggered spellhitter");
+			//Create reflected fireball if it was hit hard enough by the spell hitter.
+			Rigidbody otherBody = other.GetComponent<Rigidbody>();
 
-            print("VELOCITY: " + otherBody.velocity.magnitude + " | ANGULAR V: " + otherBody.angularVelocity.magnitude);
-            if (otherBody.velocity.magnitude > minLinearVelocity || otherBody.angularVelocity.magnitude > minAngularVelocity)
-            {
-                print("Invert Fireball!");
-                //this.transform.rotation = Quaternion.LookRotation(this.transform.forward * -1, this.transform.up * -1);
-                //this.GetComponent<Rigidbody>().velocity *= -1;
-                //StartRecovery();
-                //GameObject reflectedFireball = PhotonNetwork.Instantiate("Fireball", this.transform.position, Quaternion.LookRotation(otherBody.transform.forward, otherBody.transform.up), 0);
-                fbCollider.enabled = false;
-                GameObject reflectedFireball = PhotonNetwork.Instantiate("Fireball", this.transform.position + this.transform.forward * -1, Quaternion.LookRotation(this.transform.forward * -1, this.transform.up * -1), 0);
-                DestroyFireball();
-                if (deflectAudio != null) audioSource.PlayOneShot(deflectAudio);
-            }
+			print("VELOCITY: " + otherBody.velocity.magnitude + " | ANGULAR V: " + otherBody.angularVelocity.magnitude);
+			if (otherBody.velocity.magnitude > minLinearVelocity || otherBody.angularVelocity.magnitude > minAngularVelocity)
+			{
+				print("Invert Fireball!");
+				//this.transform.rotation = Quaternion.LookRotation(this.transform.forward * -1, this.transform.up * -1);
+				//this.GetComponent<Rigidbody>().velocity *= -1;
+				//StartRecovery();
+				//GameObject reflectedFireball = PhotonNetwork.Instantiate("Fireball", this.transform.position, Quaternion.LookRotation(otherBody.transform.forward, otherBody.transform.up), 0);
+				fbCollider.enabled = false;
+				GameObject reflectedFireball = PhotonNetwork.Instantiate("Fireball", this.transform.position + this.transform.forward * -1, Quaternion.LookRotation(this.transform.forward * -1, this.transform.up * -1), 0);
+				DestroyFireball();
+				if (deflectAudio != null) audioSource.PlayOneShot(deflectAudio);
+			}
 
-        }
-    }
-    private void StartRecovery()
-    {
-        activeTimer = startup;
-        fbCollider.enabled = false;
-    }
-    private void DestroyFireball()
-    {
-        //Destroy game object.
-//        PhotonNetwork.Destroy(this.gameObject);
+		}
+	}
+	private void StartRecovery()
+	{
+		activeTimer = startup;
+		fbCollider.enabled = false;
+	}
+	private void DestroyFireball()
+	{
+		//Destroy game object.
+		//        PhotonNetwork.Destroy(this.gameObject);
+		spellcastingGesture.enabled = true;
 		Destroy(reticleInstance);
 		Destroy(this.gameObject);
-    }
+	}
 
 }
