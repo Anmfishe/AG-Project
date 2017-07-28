d0ef2de68ca6f4d272eda91953d3cb400a032651
Anmfishe <anmfishe@ucsc.edu>
2017-07-27 18:21:18 -0700

    Iceball merge
    New Iceball spell

---
+++ Assets/Scripts/IceBall_1.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IceBall_1 : MonoBehaviour {
+    public GameObject IceBall_2;
+    float speed = 7.5f;
+    int damage = 10;
+    PhotonView photonView;
+	// Use this for initialization
+	void Start () {
+        photonView = GetComponent<PhotonView>();
+        StartCoroutine(lifetime());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyUp("joystick button 15") && photonView.isMine) 
+        {
+            PhotonNetwork.Instantiate(IceBall_2.name, transform.position, Quaternion.identity, 0);
+            PhotonNetwork.Destroy(photonView);
+        }
+	}
+    private void FixedUpdate()
+    {
+        this.transform.Translate(this.transform.forward * speed * Time.deltaTime, Space.World);
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Shield"))
+        {
+            print("hit on shield");
+            //Apply damage to the shield.
+            Damageable damageScript = collision.gameObject.GetComponent<Damageable>();
+            if (damageScript != null) damageScript.TakeDamage(damage);
+            //Instantiate new explosion.
+            PhotonNetwork.Destroy(photonView);
+        }
+    }
+    IEnumerator lifetime()
+    {
+        yield return new WaitForSeconds(10);
+        PhotonNetwork.Instantiate(IceBall_2.name, transform.position, Quaternion.identity, 0);
+        PhotonNetwork.Destroy(photonView);
+    }
+
+}
---
+++ Assets/Scripts/IceBall_2.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IceBall_2 : MonoBehaviour {
+    ParticleSystem ps;
+    PhotonView photonView;
+    bool doDamage = true;
+	// Use this for initialization
+	void Start () {
+        ps = GetComponentInChildren<ParticleSystem>();
+        photonView = GetComponent<PhotonView>();
+        StartCoroutine(AOE_Timer());
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		if(doDamage)
+        {
+            
+            RaycastHit[] hits;
+            
+        }
+	}
+    IEnumerator AOE_Timer()
+    {
+        Wave();
+        yield return new WaitForSeconds(1);
+        Wave();
+        yield return new WaitForSeconds(1);
+        Wave();
+        yield return new WaitForSeconds(1);
+        Wave();
+        yield return new WaitForSeconds(1);
+        ps.Stop();
+        yield return new WaitForSeconds(4);
+        PhotonNetwork.Destroy(photonView);
+    }
+    void Wave()
+    {
+        Collider[] hits;
+        hits = Physics.OverlapSphere(transform.position, 5);
+        foreach (Collider hit in hits)
+        {
+            if (hit.transform.tag == "Player")
+            {
+                hit.transform.GetComponent<PlayerStatus>().takeDamage(10);
+            }
+        }
+    }
+}
