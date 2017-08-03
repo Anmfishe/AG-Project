a41d655eeb547f82029e6ff5a263269047e49dc0
Anmfishe <anmfishe@ucsc.edu>
2017-08-03 15:05:55 -0700

    One more quick fix

--- Assets/Scripts/NetworkManager.cs
+++ Assets/Scripts/NetworkManager.cs
@@ -193,7 +193,7 @@ public class NetworkManager : Photon.PunBehaviour
 		cameraRig.GetComponent<SpellcastingGestureRecognition>().SetAvatar(avatar.transform);
         cameraRig.GetComponent<PlatformController>().SetAvatar(avatar);
         
-        if (PunTeams.PlayersPerTeam[PunTeams.Team.red].Count >= PunTeams.PlayersPerTeam[PunTeams.Team.blue].Count)
+        if (PunTeams.PlayersPerTeam[PunTeams.Team.blue].Count >= PunTeams.PlayersPerTeam[PunTeams.Team.red].Count)
         {
             avatar.GetComponent<TeamManager>().SetRed();
             PhotonNetwork.player.SetTeam(PunTeams.Team.red);
