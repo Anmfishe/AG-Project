28be135870bde10bf6b7119b7861ee1c1c593d89
dylfaust <mdfaust@ucsc.edu>
2017-08-10 04:38:59 -0700

    Fixed scoreboard to have hearts.

--- Assets/Scripts/NetworkManager.cs
+++ Assets/Scripts/NetworkManager.cs
@@ -230,6 +230,7 @@ public class NetworkManager : Photon.PunBehaviour
         {
             roundMan = PhotonNetwork.InstantiateSceneObject(this.roundMan.name, new Vector3(0, 0, 0), Quaternion.identity, 0, null);
             roundMan.GetComponent<RoundManager>().Subscribe(avatar, cameraRig);
+         
             //powerupManager = PhotonNetwork.InstantiateSceneObject(this.powerupManager.name, new Vector3(0, 0, 0), Quaternion.identity, 0, null);
         }
         else
