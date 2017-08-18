7e28672eda1d27987da529a4446ab96f003e6d24
STREAM-MONSTER\Student <anmfishe@ucsc.edu>
2017-08-18 04:38:02 -0700

    - created notification manager as child on main camera

---
+++ Assets/NotificationManager.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NotificationManager : MonoBehaviour {
+
+    TextMesh notification;
+    float timer = 101;
+
+	// Use this for initialization
+	void Start () {
+        if (this.transform.Find("NotificationText") != null)
+        {
+            notification = this.transform.Find("NotificationText").GetComponent<TextMesh>();
+        }
+        else
+        {
+            Debug.Log("NotificationManager.cs : Start() : Could not find child \"NotificationText\"");
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (timer <= 0)
+        {
+            Clear();
+        }
+        else if (timer == 101)
+        {
+            // do nothing, keep notification displayed indefinitely
+        }
+        else
+        {
+            timer -= Time.deltaTime;
+        }
+	}
+
+    public void SetNotification(string text, float duration = 3)
+    {
+        if (notification == null)
+        {
+            return;
+        }
+
+        notification.text = text;
+        timer = duration;
+    }
+
+    public void Clear()
+    {
+        notification.text = "";
+    }
+}
